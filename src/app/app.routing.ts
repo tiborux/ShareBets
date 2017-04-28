932cc97b5562d0c6a6d21203f7b4159f4f6e8fb9
fagarbal <fagarbal@gmail.com>
2017-04-28 17:14:17 +0200

    second try

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from "@angular/core";
 
 export const routes:Routes=[
-  { path: 'app', component: AppComponent,
-    children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'inicio', component: ContentHomeComponent }
-    ]
-  },
-  { path: '', redirectTo:'inicio',pathMatch: 'full' }
+ { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: ContentHomeComponent },
+  { path: '', component: AppComponent, redirectTo:'inicio',pathMatch: 'full' }
 ]
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
